42a11e00eb780910be4b419bb979d3c5bd3b0019
sondos alansi <sondos.alansi@yahoo.com>
2018-09-25 20:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here:
+function driversWithRevenueOver(driver,revenue){
+ return driver.filter(function(driver){
+    
+    return driver.revenue>revenue;
+  })
+  
+}
+
+function driverNamesWithRevenueOver(driver,revenue){
+
+  let d1= driver.filter(function(driver){
+    
+  
+   
+   return  driver.revenue>revenue;
+      
+      
+    });
+    let d2= d1.map(function(d1){
+       
+       return d1.name;
+     });
+    
+    return d2;
+}
+
+
+
+function exactMatch(driver,prop){
+  return driver.filter(function(driver){
+     return driver.key===prop;
+  });
+  
+}
